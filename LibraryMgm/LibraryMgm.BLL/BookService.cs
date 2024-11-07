04e994d9c569a6b2bda5fb4882148e836878c6dd
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-07 18:05:29 +0330

    add "operation result", "Book Service"

---
+++ LibraryMgm/LibraryMgm.BLL/BookService.cs
@@ -0,0 +1,27 @@
+﻿using LibraryMgm.Model.Entities;
+
+namespace LibraryMgm.BLL
+{
+    public class BookService
+    {
+        public OperationResult Insert(Book book)
+        {
+            return new OperationResult();
+        }
+
+        public OperationResult Update(Book book)
+        {
+            return new OperationResult();
+        }
+
+        public OperationResult Delete()
+        {
+            return new OperationResult();
+        }
+
+        public OperationResult Select()
+        {
+            return new OperationResult();
+        }
+    }
+}
