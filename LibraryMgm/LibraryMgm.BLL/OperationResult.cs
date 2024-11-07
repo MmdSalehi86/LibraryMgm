a033ffabd3120c0efc7d4f443e047d451bd43e2b
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-07 18:05:29 +0330

    add "operation result", "Book Service"

---
+++ LibraryMgm/LibraryMgm.BLL/OperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Policy;
+
+namespace LibraryMgm.BLL
+{
+    public class OperationResult
+    {
+        public bool ExcSucc { get; set; }
+        public bool IsValid { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class OperationResult<T> : OperationResult
+    {
+        public T Data;
+    }
+}
