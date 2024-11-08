a011a12fd8d2c4ec56163558ce302759df1e7d7a
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-08 13:00:28 +0330

    add Translator view and insert model

---
+++ LibraryMgm/LibraryMgm.Model/TranslatorModel/InsertTranslatorModel.cs
@@ -0,0 +1,19 @@
+﻿using LibraryMgm.Model.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryMgm.Model.Entities
+{
+    public class InsertTranslatorModel : BaseValidation
+    {
+        [Required(ErrorMessage = "وارد کردن نام مترجم اجباری است")]
+        [MaxLength(32, ErrorMessage = "نام ناید از 32 کاراکتر بیشتر باشد")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "وارد کردن نام خانوادگی مترجم اجباری است")]
+        [MaxLength(32, ErrorMessage = "نام خانوادگی ناید از 32 کاراکتر بیشتر باشد")]
+        public string LastName { get; set; }
+
+        [MaxLength(128, ErrorMessage = "نام زندگی نباید بیشتر از 128 کاراکتر باشد")]
+        public string Location { get; set; }
+    }
+}
---
+++ LibraryMgm/LibraryMgm.Model/TranslatorModel/TranslatorVM.cs
@@ -0,0 +1,20 @@
+﻿using LibraryMgm.Model.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryMgm.Model.Entities
+{
+    public class TranslatorVM : BaseValidation
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName { get {  return $"{FirstName} {LastName}"; } }
+
+        public string Location { get; set; }
+    }
+}
