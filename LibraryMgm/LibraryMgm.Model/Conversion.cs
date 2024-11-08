6098c4a363f182ad8ce4fb68d50e5179bda1f3f3
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-08 19:01:35 +0330

    add simple conversion

---
+++ LibraryMgm/LibraryMgm.Model/Conversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace LibraryMgm.Model.Conversion
+{
+    public static class Conversion
+    {
+        public static T ToViewModel<T>(this SqlDataReader reader)
+        {
+            T vm;
+            var classType = typeof(T);
+            vm = (T)Activator.CreateInstance(classType);
+
+            var objType = vm.GetType();
+            var props = objType.GetProperties();
+
+            foreach (var prop in props)
+            {
+                //foreach (var value in prop.CustomAttributes.Any())
+                {
+                    prop.CustomAttributes.Any((a) => { return a.AttributeType.Name == "cc"; });
+                    //if (prop.Attributes. == null) ;
+                }
+            }
+
+            return vm;
+        }
+    }
+}
