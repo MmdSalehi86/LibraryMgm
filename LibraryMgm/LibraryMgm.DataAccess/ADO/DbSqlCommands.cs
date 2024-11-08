c7a956b4fdc49e237abf0d7ccbb995faade2ff2f
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-08 18:08:04 +0330

    add "Db Sql Commands", "ConnectionString"

---
+++ LibraryMgm/LibraryMgm.DataAccess/ADO/DbSqlCommands.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LibraryMgm.DataAccess.ADO
+{
+    public class DbSqlCommands
+    {
+        private SqlConnection ConnectToDb()
+        {
+            SqlConnection con = new SqlConnection(Connections.Conn_LibMgmDb);
+            con.Open();
+            return con;
+        }
+
+        protected int ExcNonQueryProc(string proc, SqlParameter[] ps = null)
+        {
+            SqlCommand cmd = new SqlCommand();
+            if (ps != null)
+                foreach (var p in ps)
+                    cmd.Parameters.Add(p);
+
+            int result;
+            using (cmd.Connection = ConnectToDb())
+            {
+                cmd.CommandText = proc;
+                cmd.CommandType = CommandType.StoredProcedure;
+                result = cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+            }
+            return result;
+        }
+
+        protected T ExcScalarProc<T>(string proc, SqlParameter[] ps = null)
+        {
+            SqlCommand cmd = new SqlCommand();
+            if (ps != null)
+                foreach (var p in ps)
+                    cmd.Parameters.Add(p);
+
+            T result;
+            using (cmd.Connection = ConnectToDb())
+            {
+                cmd.CommandText = proc;
+                cmd.CommandType = CommandType.StoredProcedure;
+                result = (T)cmd.ExecuteScalar();
+                cmd.Connection.Close();
+            }
+            return result;
+        }
+
+        protected SqlDataReader ExcReaderProc(string proc, SqlParameter[] ps = null)
+        {
+            SqlCommand cmd = new SqlCommand();
+            if (ps != null)
+                foreach (var p in ps)
+                    cmd.Parameters.Add(p);
+
+            SqlDataReader result;
+            using (cmd.Connection = ConnectToDb())
+            {
+                cmd.CommandText = proc;
+                cmd.CommandType = CommandType.StoredProcedure;
+                result = cmd.ExecuteReader();
+                cmd.Connection.Close();
+            }
+            return result;
+        }
+
+        protected T ExecScalarFunc<T>(string funcName, SqlParameter[] ps = null)
+        {
+            SqlCommand cmd = new SqlCommand();
+            string commandText = "select " + funcName + "(";
+            if (ps != null)
+            {
+                foreach (var p in ps)
+                {
+                    cmd.Parameters.Add(p);
+                    commandText += p.ParameterName + ",";
+                }
+            }
+            T result;
+            using (cmd.Connection = ConnectToDb())
+            {
+                commandText = commandText.Remove(commandText.Length - 1, 1);
+                commandText += ")";
+                cmd.CommandText = commandText;
+                result = (T)cmd.ExecuteScalar();
+                cmd.Connection.Close();
+            }
+            return result;
+        }
+
+        protected SqlDataReader ExecReaderFunc<T>(string funcName, SqlParameter[] ps = null)
+        {
+            SqlCommand cmd = new SqlCommand();
+            string commandText = "select " + funcName + "(";
+            if (ps != null)
+            {
+                foreach (var p in ps)
+                {
+                    cmd.Parameters.Add(p);
+                    commandText += p.ParameterName + ",";
+                }
+            }
+            SqlDataReader result;
+            using (cmd.Connection = ConnectToDb())
+            {
+                commandText = commandText.Remove(commandText.Length - 1, 1);
+                commandText += ")";
+                cmd.CommandText = commandText;
+                result = cmd.ExecuteReader();
+                cmd.Connection.Close();
+            }
+            return result;
+        }
+    }
+}
---
+++ LibraryMgm/LibraryMgm.DataAccess/Connections.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace LibraryMgm.DataAccess
+{
+    public class Connections
+    {
+        public static string Conn_LibMgmDb { get
+            {
+                return ConfigurationManager.ConnectionStrings["Conn_LibMgmDb"].ConnectionString;
+            }
+        }
+    }
+}
