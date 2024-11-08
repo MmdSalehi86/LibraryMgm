12b7e9fcbc3adbffd10ed18bf2d5aa46b2895061
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-08 13:03:13 +0330

    Create "Translator Service"

---
+++ LibraryMgm/LibraryMgm.BLL/TranslatorService.cs
@@ -0,0 +1,28 @@
+﻿using LibraryMgm.Model.BookModel;
+using LibraryMgm.Model.Entities;
+
+namespace LibraryMgm.BLL
+{
+    public class TranslatorService
+    {
+        public OperationResult Insert(InsertTranslatorModel book)
+        {
+            return new OperationResult();
+        }
+
+        public OperationResult Update(Translator book)
+        {
+            return new OperationResult();
+        }
+
+        public OperationResult Delete(int id)
+        {
+            return new OperationResult();
+        }
+
+        public OperationResult<TranslatorVM> Select()
+        {
+            return new OperationResult<TranslatorVM>();
+        }
+    }
+}
