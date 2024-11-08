e8ef7f2b6b01fc2b9096231b0afc9f70d8837f56
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-08 21:46:53 +0330

    Add "Repo"; Fix "DbSqlCommands"

---
+++ LibraryMgm/LibraryMgm.DataAccess/BookRepo.cs
@@ -0,0 +1,40 @@
+﻿using LibraryMgm.DataAccess.ADO;
+using LibraryMgm.Model.BookModel;
+using LibraryMgm.Model.Entities;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace LibraryMgm.DataAccess
+{
+    public class BookRepo : DbSqlCommands
+    {
+        public BookRepo()
+        {
+        }
+
+        public void Insert(InsertBookModel model)
+        {
+            //TODO: need to Entity to SqlParameter
+            ExcNonQueryProc("INSERT_BOOK",
+                null);
+        }
+
+        public List<TranslatorVM> Select()
+        {
+            return ExcReaderFunc("SELECT_BOOK").ToViewModel();
+        }
+
+        public void Update(Translator translator)
+        {
+            //TODO: need to Entity to SqlParameter
+            ExcNonQueryProc("UPDATE_BOOK", null
+                );
+        }
+
+        public void Delete(int id)
+        {
+            ExcNonQuerySql("DELETE FROM Book WHERE Id=@Id",
+                new SqlParameter("Id", id));
+        }
+    }
+}
