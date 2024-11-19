a6f0ed212a2130687b3b0f2b3b02fc0cc7e2a96d
MmdSalehi86 <m.salehi.zarei.86@gmail.com>
2024-11-19 19:19:46 +0330

    Add "Data Context"

---
+++ LibraryMgm/LibraryMgm.DataAccess/EF/LibMgmDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using LibraryMgm.Model.Entities;
+
+namespace School.DataAccess
+{
+    public class LibMgmDataContext : DbContext
+    {
+        public LibMgmDataContext() : base("Conn_LibMgmDb")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Translator> Translators { get; set; }
+    }
+}
